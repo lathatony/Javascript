b328a8b40984b75cb68c6c4c02a256e8fc04a127
lathatony <lathahanumanthu172@gmail.com>
2024-12-28 11:10:02 +0530

    JSONObject

---
+++ JSONObject.js
@@ -0,0 +1,26 @@
+
+// s1 --> Name , Age
+
+// s2 --> Name , Email
+
+// s3 --> Name, Work_Experience [XYZ: 3 Months, ABC: 12]
+
+const s1 = {
+    Name : "Priya",
+    Age : 24, 
+}
+
+const s2 = {
+    Name : "Ankit",
+    Email: 'ankit@kod.com'
+}
+
+const s3 = {
+    Name : "Akash",
+    Work_Experience : {
+        XYZ : '3 Months',
+        ABC : '12 Months'
+    }
+}
+
+console.log(s1, typeof s1)
